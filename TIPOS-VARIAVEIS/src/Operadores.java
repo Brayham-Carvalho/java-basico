429568017bfdc543725566c4eb10d4ebf2688e88
Brayham-Carvalho <chemicalbpc@gmail.com>
2023-03-02 16:06:47 -0300

    Operadores

--- TIPOS-VARIAVEIS/src/Operadores.java
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -1,21 +1,45 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM " + "JAVA";
-        System.out.println(nomeCompleto);
         
-        String concatenacao = "?";
+        boolean condicao1 = true;
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das das condições é verdadeira");
+
+        }
+        System.out.println("fim");
+
+        String nomeUm = "Brayham";
+        String nomeDois = "Brayham";
+
+        System.out.println(nomeUm.equals(nomeDois));
         
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
+        
+        
+        int numero1 = 1;
+        int numero2 = 2;
 
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
+        boolean simNao = numero1 == numero2;
+        
+        if(numero1 < numero2){
+            System.out.println("a nossa condição é verdadeira!");
+        }
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
 
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        simNao = numero1 != numero2;
 
+        System.out.println("numeroUm é diferente de numeroDois?" + simNao);
+        
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeroDois?" + simNao);
     }
 }
